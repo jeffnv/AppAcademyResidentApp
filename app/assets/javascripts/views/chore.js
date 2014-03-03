f1f37f3d4b06b4cf7d4543819a4ae818ac4bbea2
Jeffrey Fiddler <jeffnv@gmail.com>
2014-03-03 11:30:20 -0800

    update readme

--- app/assets/javascripts/views/chore.js
+++ app/assets/javascripts/views/chore.js
@@ -1,11 +1,18 @@
 AppResidentApp.Views.Chore = Backbone.View.extend({
   tagName: 'li',
   template: JST['chores/chore'],
-  render: function(){
+  events: {
+    'click a.chore-link': 'choreClicked'
+  },
+  render: function () {
     this.$el.html(this.template({
       choreName: this.model.escape('name')
     }));
 
     return this;
-  }
+  },
+
+  choreClicked: function (event) {
+    alert('clicked ' + this.model.escape('name'));
+  },
 });
